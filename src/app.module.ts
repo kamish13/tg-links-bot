bf25a1a4d5de503693911f805d57e407c5774bfd
kamish13 <78052474+kamish13@users.noreply.github.com>
2024-09-21 18:56:46 +0200

    feat: add Links module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LinksModule } from './links/links.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'links.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    LinksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
