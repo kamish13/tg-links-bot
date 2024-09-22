4e7c3b5d8b438652907907b007d58503b6e2e523
kamish13 <78052474+kamish13@users.noreply.github.com>
2024-09-22 13:06:24 +0200

    feat: add CreateLinkDto and implement link creation logic in LinksService

--- src/links/links.service.ts
+++ src/links/links.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateLinkDto } from './dto/create-link.dto';
+import { Link } from './entities/link.entity';
 
 @Injectable()
-export class LinksService {}
+export class LinksService {
+  constructor(
+    @InjectRepository(Link)
+    private linkRepository: Repository<Link>,
+  ) {}
+
+  async create(createLinkDto: CreateLinkDto): Promise<Link> {
+    const { url } = createLinkDto;
+
+    const link = new Link();
+    link.url = url;
+    link.code = await this.generateUniqueCode();
+
+    return this.linkRepository.save(link);
+  }
+
+  private async generateUniqueCode(): Promise<string> {
+    let code: string;
+    let linkWithCode: Link | null;
+
+    do {
+      code = Math.random().toString(36).substring(2, 8);
+      linkWithCode = await this.linkRepository.findOne({ where: { code } });
+    } while (linkWithCode);
+
+    return code;
+  }
+}
