047056d722b313b668fdf9cc12adac91d7a56b23
kamish13 <78052474+kamish13@users.noreply.github.com>
2024-09-22 12:39:27 +0200

    feat: add Link entity with URL, unique code, and timestamps

---
+++ src/links/link.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  Column,
+  Index,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { IsUrl } from 'class-validator';
+
+@Entity()
+export class Link {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @IsUrl()
+  url: string;
+
+  @Index()
+  @Column({ unique: true })
+  code: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
--- src/links/links.module.ts
+++ src/links/links.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LinksController } from './links.controller';
 import { LinksService } from './links.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Link } from './link.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Link])],
   controllers: [LinksController],
-  providers: [LinksService]
+  providers: [LinksService],
 })
 export class LinksModule {}
